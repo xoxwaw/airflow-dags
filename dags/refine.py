cb644fcdf50c2d16bbc09d7e0c9be460c71af806
phi <phi@honestbank.com>
2022-09-21 11:40:12 +0700

    example dag

---
+++ dags/refine.py
@@ -0,0 +1,30 @@
+import airflow
+from airflow.operators.bash_operator import BashOperator
+from airflow import DAG
+import os
+
+
+# Global variables that are set using environment varaiables
+DBT_ROOT_PATH = os.getenv('DBT_ROOT_PATH')
+
+
+default_args = {
+    "owner": "Airflow",
+    "start_date": airflow.utils.dates.days_ago(1)
+}
+
+# The DAG definition
+dag = DAG(
+    dag_id='ge_tutorials_dag_no_ge',
+    default_args=default_args,
+    schedule_interval=None,
+)
+
+task_transform_data_in_db = BashOperator(
+    task_id='task_transform_data_in_db',
+    bash_command='dbt run --project-dir {}'.format(DBT_ROOT_PATH),
+    dag=dag)
+
+
+# DAG dependencies
+task_transform_data_in_db
