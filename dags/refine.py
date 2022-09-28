fa21e8f28a9ea19146170fa3d485da9230d4a1fa
phi <phi@honestbank.com>
2022-09-28 16:35:03 +0700

    set schedule

--- dags/refine.py
+++ dags/refine.py
@@ -1,6 +1,8 @@
 import airflow
 from airflow.operators.bash_operator import BashOperator
 from airflow import DAG
+from datetime import datetime, timedelta
+
 import os
 
 
@@ -10,14 +12,18 @@ DBT_ROOT_PATH = os.getenv('DBT_ROOT_PATH')
 
 default_args = {
     "owner": "Airflow",
-    "start_date": airflow.utils.dates.days_ago(1)
+    "start_date": airflow.utils.dates.days_ago(1),
+    'email': ['phi@honestbank.com'],
+    'email_on_failure': True,
+    'email_on_success': True,
 }
 
 # The DAG definition
 dag = DAG(
-    dag_id='ge_tutorials_dag_no_ge',
+    dag_id='refine dbt',
     default_args=default_args,
-    schedule_interval=None,
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2022, 9, 27),
 )
 
 task_transform_data_in_db = BashOperator(
